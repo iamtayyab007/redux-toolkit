09c8fd254866296c2441eb10b65f915c6fdfd545
iamtayyab007 <tayyabm336@gmail.com>
2024-06-23 01:55:44 +0500

    react-redux project completed

--- src/Components/AddTodo.jsx
+++ src/Components/AddTodo.jsx
@@ -1,5 +1,84 @@
-import React from "react";
+{
+  /*import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../features/todo/todoSlice";
 
 export const AddTodo = () => {
-  return <div>AddTodo</div>;
+  const dispatch = useDispatch();
+  const [input, setInput] = useState("");
+
+  const addTodoHandler = (e) => {
+    e.preventDefault();
+    dispatch(addTodo(input));
+    setInput("");
+  };
+  return (
+    <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
+      <input
+        type="text"
+        className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+        placeholder="Enter a Todo..."
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      />
+      <button
+        type="submit"
+        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+      >
+        Add Todo
+      </button>
+    </form>
+  );
 };
+*/
+}
+
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo, updateTodo } from "../features/todo/todoSlice";
+
+function AddTodo() {
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch();
+  const editingTodo = useSelector((state) => state.editingTodo);
+  useEffect(() => {
+    if (editingTodo) {
+      setInput(editingTodo.text);
+    } else {
+      setInput("");
+    }
+  }, [editingTodo]);
+
+  const addTodoHandler = (e) => {
+    e.preventDefault();
+    if (editingTodo) {
+      dispatch(updateTodo({ id: editingTodo.id, text: input }));
+    } else {
+      dispatch(addTodo(input));
+      setInput("");
+    }
+  };
+
+  return (
+    <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
+      <input
+        type="text"
+        className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+        placeholder="Enter a Todo..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button
+        type="submit"
+        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+      >
+        {editingTodo ? "Update Todo" : "Add Todo"}
+      </button>
+    </form>
+  );
+}
+
+export default AddTodo;
--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -1,4 +1,5 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+{
+  /*import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const intitialState = {
   todos: [{ id: 1, text: "Hi World" }],
@@ -21,12 +22,54 @@ export const todoSlice = createSlice({
 
     updateTodo: (state, action) => {
       state.todos = state.todos.map((todo) =>
-        todo.id === action.payload ? todo : action.payload
+        todo.id === action.payload ? action.payload : todo
       );
     },
   },
 });
 
-export const { addTodo, removeTodo } = todoSlice.actions;
+export const { addTodo, removeTodo, updateTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
+*/
+}
+
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [{ id: 1, text: "Hello world" }],
+  editingTodo: null,
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    updateTodo: (state, action) => {
+      state.todos = state.todos.map((todo) =>
+        todo.id === action.payload.id
+          ? { ...todo, text: action.payload.text }
+          : todo
+      );
+      state.editingTodo = null;
+    },
+    setEditingTodo: (state, action) => {
+      state.editingTodo = action.payload;
+    },
+  },
+});
+
+export const { addTodo, removeTodo, updateTodo, setEditingTodo } =
+  todoSlice.actions;
 
 export default todoSlice.reducer;
